53867be3f4eb164508afc7efab39998ff39df304
Marco Antônio da Veiga de Andrade <marquinhosandrad@outlook.com>
2022-11-17 16:19:16 -0300

    Aula 10/11/2022

--- site-react/src/App.jsx
+++ site-react/src/App.jsx
@@ -1,28 +1,28 @@
-import { Route, Routes } from "react-router-dom";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { HomePage } from "./pages/Home";
+import { Route, Routes } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { HomePage } from './pages/Home';
 
 import './assets/css/normalize.css';
 import './assets/css/global.css';
 
 function App() {
 
-  return (
-    <div className="App">
-      <Header />
+    return (
+        <div className="App">
 
-      <main>
-        <Routes>
-          <Route path="*" element={<HomePage />} />
-        </Routes>
-      </main>
+            <Header />
 
-      <Footer />
+            <main>
+                <Routes>
+                    <Route path="*" element={<HomePage />} />
+                </Routes>
+            </main>
 
+            <Footer />
 
-    </div>
-  )
-
+        </div>
+    )
 }
+
 export default App
