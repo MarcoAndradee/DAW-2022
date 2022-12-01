167a1f81905eeb1263c6ef72c171ad66af36e227
Marco Antônio da Veiga de Andrade <marquinhosandrad@outlook.com>
2022-12-01 19:25:33 -0300

    Aula 01/12/2022

--- atv-react/src/pages/Home/index.jsx
+++ atv-react/src/pages/Home/index.jsx
@@ -2,8 +2,6 @@ import{Banner} from '../../components/Banner';
 import {Cards} from '../../components/Cards';
 import {Cupons} from '../../components/Cupons';
 import {E_commerce} from '../../components/E_commerce';
-import {Footer} from '../../components/Footer'
-import {Header} from '../../components/Header';
 export function HomePage() {
     return(
         <div id="home-page">
@@ -12,9 +10,7 @@ export function HomePage() {
                 <Cards/>
                 <Cupons/>
                 <E_commerce/>
-                <Footer/>
-                <Header/>
-
+                
             </div>
         </div>
     )
